d854bdae344f401a5892a9a1e3817fd879e482ee
heiNina <nina.shabrova2001@gmail.com>
2020-08-26 02:38:07 +0300

    Add image uploading

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
 import {FirebaseDatabaseMutation} from '@react-firebase/database'
-import {Input, Button} from 'antd'
+import {Button, Input} from 'antd'
 import styles from './Form.module.css'
+import {ImageField} from '../ImageField'
+
 
 const style = {
     width: '200px'
 }
 
 export const Form = () => {
-    const {register, handleSubmit} = useForm()
+    const {register, handleSubmit} = useForm({shouldUnregister: true})
 
-    const registerAntdInput = ({input}) => register(input)
+    const registerAntdInput = ({input}) => register({
+        required: true,
+    })(input)
 
     return (
         <FirebaseDatabaseMutation type="push" path="ScentHunt/products">{
             ({runMutation}) => (
                 <form onSubmit={handleSubmit(runMutation)} className={styles.form}>
                     <Input style={style} placeholder="name" name="name" ref={registerAntdInput}/>
-                    <Input style={style} placeholder="image" name="image" ref={registerAntdInput}/>
+                    <ImageField register={register} style={style}/>
                     <Input style={style} placeholder="brand" name="brand" ref={registerAntdInput}/>
                     <Input style={style} placeholder="price" name="price" ref={registerAntdInput}/>
                     <Input style={style} placeholder="uniq" name="uniq" ref={registerAntdInput}/>
