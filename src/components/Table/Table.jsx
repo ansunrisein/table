bdb5734f291e4722fafc044b35e95555ceafe1bd
heiNina <nina.shabrova2001@gmail.com>
2020-08-26 01:01:09 +0300

    Add Table

---
+++ src/components/Table/Table.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {FirebaseDatabaseNode} from '@react-firebase/database'
+import {Table as AntTable} from 'antd'
+import styles from './Table.module.css'
+
+
+export const Table = () => {
+    const path = 'ScentHunt/products'
+
+    const s = obj => Object.keys(obj).reduce((acc, key) => [...acc, {id: key, ...obj[key]}], [])
+
+    return (
+        <FirebaseDatabaseNode path={path} orderByKey>{d => (
+            d.isLoading
+                ? <span>Load...</span>
+                : <AntTable dataSource={d.value && s(d.value)} columns={columns}/>
+        )}</FirebaseDatabaseNode>
+    )
+}
+
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'Image',
+        dataIndex: 'image',
+        key: 'image',
+        width: '20%',
+        render:  (url) => <img className={styles.image} src={url} alt=""/>
+    },
+    {
+        title: 'Brand',
+        dataIndex: 'brand',
+        key: 'brand'
+    },
+    {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price'
+    },
+    {
+        title: 'Uniq',
+        dataIndex: 'uniq',
+        key: 'uniq'
+    },
+    {
+        title: 'Long',
+        dataIndex: 'long',
+        key: 'long'
+    },
+    {
+        title: 'Ingredient',
+        dataIndex: 'ingredient',
+        key: 'ingredient'
+    }
+]
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Form} from '../../components'
+import {Form, Table} from '../../components'
 import styles from './Main.module.css'
 
 
 export const Main = () => (
     <main className={styles.main}>
         <Form/>
+        <Table/>
     </main>
 )
