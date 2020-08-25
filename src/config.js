fff431062293c317215b3f0f8abc8148da85c9b1
heiNina <nina.shabrova2001@gmail.com>
2020-08-25 22:02:49 +0300

    Add config for Firebase

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: 'AIzaSyBiYaLSb5xnTBKX9yzTHoqjJN53ANoJxGU',
+    authDomain: 'scenthunt.firebaseapp.com',
+    databaseURL: 'https://scenthunt.firebaseio.com',
+    projectId: 'scenthunt',
+    storageBucket: 'scenthunt.appspot.com',
+    messagingSenderId: '869815807272',
+    appId: '1:869815807272:web:80f76c3d90f523fd66741d',
+    measurementId: 'G-4ZP7R4JST1'
+}
