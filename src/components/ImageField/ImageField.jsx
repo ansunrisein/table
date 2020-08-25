e05199cc74c06c6fcf40eaa9fc04de26d68d6e25
heiNina <nina.shabrova2001@gmail.com>
2020-08-26 02:37:58 +0300

    Add ImageField

---
+++ src/components/ImageField/ImageField.jsx
@@ -0,0 +1,26 @@
+import React, {useMemo, useState} from 'react'
+import firebase from 'firebase'
+import {nanoid} from 'nanoid'
+
+
+export const ImageField = ({register, style}) => {
+    const storage = useMemo(() => firebase.storage(), [])
+    const [image, setImage] = useState('')
+
+    const uploadImage = async e => {
+        const file = e.target.files[0]
+        const name = 'products/image-' + nanoid()
+
+        await storage.ref(name).put(file)
+
+        const url = await storage.ref(name).getDownloadURL()
+        setImage(url)
+    }
+
+    return (
+        <>
+            <input style={style} placeholder="image" type="file" onChange={uploadImage}/>
+            <input hidden readOnly name="image" ref={register} value={image}/>
+        </>
+    )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export * from './Form'
 export * from './Table'
+export * from './ImageField'
