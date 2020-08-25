318e50f6bc03c0e65a0d84ece0bdc96e0616c9ac
heiNina <nina.shabrova2001@gmail.com>
2020-08-26 00:19:57 +0300

    Add Main page

---
+++ src/pages/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Form} from '../../components'
+import styles from './Main.module.css'
+
+
+export const Main = () => (
+    <main className={styles.main}>
+        <Form/>
+    </main>
+)
