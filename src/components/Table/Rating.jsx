8a164dee4d74c8653511b666fb697977c0a8cdde
heiNina <nina.shabrova2001@gmail.com>
2020-08-28 02:22:31 +0300

    Add rating inc

--- src/components/Table/Rating.jsx
+++ src/components/Table/Rating.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import {UpSquareFilled} from '@ant-design/icons'
 import c from 'classnames'
 import {Flex} from 'reflexbox'
 import styles from './Table.module.css'
+import {inc} from 'ramda'
 
 
-export const Rating = ({size, vertical, rating, larger}) => (
-    <Flex flexDirection={vertical && 'column'} alignItems="center">
-        <UpSquareFilled className={size === 'big' ? styles.big : styles.tiny}/>
-        <span className={c(styles.bold, larger ? styles.larger : styles.xlarge)}>{rating}</span>
-    </Flex>
-)
+export const Rating = ({size, vertical, rating, larger}) => {
+    const [rate, setRate] = useState(rating)
+
+    return (
+        <Flex flexDirection={vertical && 'column'} alignItems="center">
+            <UpSquareFilled className={c(size === 'big' ? styles.big : styles.tiny, styles.pointer)} onClick={() => setRate(inc)}/>
+            <span className={c(styles.bold, larger ? styles.larger : styles.xlarge, styles.selection)}>{rate}</span>
+        </Flex>
+    )
+}
 
 Rating.propTypes = {
     size: PropTypes.oneOf(['tiny', 'medium', 'big']).isRequired,
