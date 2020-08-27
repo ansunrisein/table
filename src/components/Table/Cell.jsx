968dc92968336add8333fd6c4496a36aca7c153d
heiNina <nina.shabrova2001@gmail.com>
2020-08-27 22:33:07 +0300

    Fix PropTypes

--- src/components/Table/Cell.jsx
+++ src/components/Table/Cell.jsx
@@ -8,16 +8,16 @@ import styles from './Table.module.css'
 
 export const Cell = ({text, image, horizontal, rating, size}) => (
     <Flex flexDirection={horizontal ? "row" : "column"} alignItems="center" justifyContent="center">
-        {text && <span className={c(styles.text, !horizontal && styles.margin, size === 'small' && styles.larger)}>{text}</span>}
+        {text && <span className={c(styles.text, !horizontal && styles.margin, size === 'tiny' && styles.larger)}>{text}</span>}
         {image &&  <img className={styles.image} src={image} alt="Image"/>}
-        <Rating rating={rating} size={size} vertical={!!image} larger={!!image || size === 'small'}/>
+        <Rating rating={rating} size={size} vertical={!!image} larger={!!image || size === 'tiny'}/>
     </Flex>
 )
 
 Rating.propTypes = {
     size: PropTypes.oneOf(['tiny', 'medium', 'big']).isRequired,
-    text: PropTypes.string.isRequired,
-    rating: PropTypes.string.isRequired,
+    text: PropTypes.string,
     image: PropTypes.string,
+    rating: PropTypes.string.isRequired,
     horizontal: PropTypes.bool,
 }
