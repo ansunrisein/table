351bdf7ef5972a8af3b7dabb7a807e24804dfed9
heiNina <nina.shabrova2001@gmail.com>
2020-08-27 21:44:48 +0300

    Add Title

---
+++ src/components/Table/Title.jsx
@@ -0,0 +1,28 @@
+import React, {useMemo} from 'react'
+import PropTypes from 'prop-types'
+import {Flex} from 'reflexbox'
+import {DownOutlined, UpOutlined} from '@ant-design/icons'
+import styles from './Table.module.css'
+
+
+export const Title = ({text, sort}) => {
+    const style = useMemo(() => ({
+        height: sort === 'none' ? '0.5em' : '1em',
+        color: sort === 'asc' ? 'green' : sort === 'desc' ? 'red' : 'gray'
+    }), [sort])
+
+    return (
+        <Flex justifyContent="center">
+            <Flex flexDirection="column" justifyContent={sort !== 'none' && 'center'} className={styles.icon}>
+                {(sort === 'asc' || sort === 'none') && <UpOutlined style={style} color="red"/>}
+                {(sort === 'desc' || sort === 'none') && <DownOutlined style={style}/>}
+            </Flex>
+            <span className={styles.uppercase}>{text}</span>
+        </Flex>
+    )
+}
+
+Title.propTypes = {
+    text: PropTypes.string.isRequired,
+    sort: PropTypes.oneOf(['asc', 'none', 'desc'])
+}
