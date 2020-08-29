7a9f420e04341dfe3de41c8522cadc4f71a232d2
heiNina <nina.shabrova2001@gmail.com>
2020-08-29 04:14:03 +0300

    Inject new components and UI

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,29 +1,56 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
-import {Button} from 'antd'
 import {Flex} from 'reflexbox'
 import {withFirebaseDatabaseMutation} from '../../hocs'
 import {ImageField} from './ImageField'
+import styles from './Form.module.css'
+import {Button, Disclaimer, Input, Logo} from '../../ui'
 
 
-const withProductsMutation = withFirebaseDatabaseMutation({type: "push", path: "ScentHunt/products"})
+const withProductsMutation = withFirebaseDatabaseMutation({type: 'push', path: 'ScentHunt/products'})
 
 
 export const Form = withProductsMutation(({runMutation}) => {
     const {register, handleSubmit} = useForm({shouldUnregister: true})
 
     return (
-        <Flex as="form" onSubmit={handleSubmit(runMutation)} flexDirection="column" justifyContent="space-between">
-            <ImageField register={register}/>
-            <input placeholder="name" name="name" ref={register}/>
-            <input placeholder="brand" name="brand" ref={register}/>
-            <input placeholder="price" name="price" ref={register}/>
-            <input placeholder="uniqueness" name="uniqueness" ref={register}/>
-            <input placeholder="longevity" name="longevity" ref={register}/>
-            <input placeholder="ingredients" name="ingredients" ref={register}/>
-            <Button type="primary" htmlType="submit">
-                Add
-            </Button>
+        <Flex as="form" flexDirection="column" justifyContent="space-between" alignItems="center"
+              className={styles.container}
+              onSubmit={handleSubmit(runMutation)}
+        >
+            <Logo className={styles.logo}/>
+            <Disclaimer className={styles.disclaimer}/>
+            <Flex justifyContent="space-around" width="100%">
+                <Flex flexDirection="column">
+                    <span className={styles.name}>IMAGE</span>
+                    <ImageField register={register}/>
+                </Flex>
+                <Flex flexDirection="column">
+                    <span className={styles.name}>NAME</span>
+                    <Input name="name" ref={register}/>
+                </Flex>
+                <Flex flexDirection="column">
+                    <span className={styles.name}>BRAND</span>
+                    <Input name="brand" ref={register}/>
+                </Flex>
+                <Flex flexDirection="column">
+                    <span className={styles.name}>PRICE</span>
+                    <Input name="price" ref={register}/>
+                </Flex>
+                <Flex flexDirection="column">
+                    <span className={styles.name}>UNIQUENESS</span>
+                    <Input name="uniqueness" ref={register}/>
+                </Flex>
+                <Flex flexDirection="column">
+                    <span className={styles.name}>LONGEVITY</span>
+                    <Input name="longevity" ref={register}/>
+                </Flex>
+                <Flex flexDirection="column">
+                    <span className={styles.name}>INGREDIENTS</span>
+                    <Input name="ingredients" ref={register}/>
+                </Flex>
+            </Flex>
+            <Button type="submit" className={styles.button}>Upload</Button>
         </Flex>
     )
 })
--- src/components/Form/ImageField.jsx
+++ src/components/Form/ImageField.jsx
@@ -1,10 +1,12 @@
 import React, {useMemo, useState} from 'react'
 import PropTypes from 'prop-types'
 import * as firebase from 'firebase'
+import {Flex} from 'reflexbox'
 import {nanoid} from 'nanoid'
+import styles from './Form.module.css'
 
 
-export const ImageField = ({register, style}) => {
+export const ImageField = ({register}) => {
     const storage = useMemo(() => firebase.storage(), [])
     const [image, setImage] = useState('')
 
@@ -19,14 +21,18 @@ export const ImageField = ({register, style}) => {
     }
 
     return (
-        <>
-            <input style={style} placeholder="image" type="file" onChange={uploadImage}/>
+        <Flex justifyContent="flex-end" className={styles.upload_container}>
+            <button className={styles.upload}>
+                <label className={styles.label} htmlFor="upload-photo">
+                    Browse
+                </label>
+            </button>
+            <input hidden type="file" name="image" id="upload-photo" className={styles.file} onChange={uploadImage}/>
             <input hidden readOnly name="image" ref={register} value={image}/>
-        </>
+        </Flex>
     )
 }
 
 ImageField.propTypes = {
     register: PropTypes.func,
-    style: PropTypes.object,
 }
