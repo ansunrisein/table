3370853951530b3051cf730fccbb5a303fc5f44f
heiNina <nina.shabrova2001@gmail.com>
2020-08-31 17:27:25 +0300

    Extract HOC into separate file

--- src/hocs/index.js
+++ src/hocs/index.js
@@ -1,15 +1,1 @@
-import React from 'react'
-import {createHoc} from './createHoc'
-
-import * as firebase from 'firebase'
-import {FirebaseAuthConsumer} from '@react-firebase/auth'
-import {FirebaseDatabaseMutation, FirebaseDatabaseNode} from '@react-firebase/database'
-import {config} from '../config'
-
-
-export const withFirebaseAuthConsumer = createHoc(FirebaseAuthConsumer)({...config, firebase: firebase})
-
-export const withFirebaseDatabaseNode = createHoc(FirebaseDatabaseNode)
-
-export const withFirebaseDatabaseMutation = createHoc(FirebaseDatabaseMutation)
-
+export * from './withFirebase'
---
+++ src/hocs/withFirebase.js
@@ -0,0 +1,14 @@
+import {createHoc} from '../utils'
+
+import * as firebase from 'firebase'
+import {FirebaseAuthConsumer} from '@react-firebase/auth'
+import {FirebaseDatabaseMutation, FirebaseDatabaseNode} from '@react-firebase/database'
+import {config} from '../config'
+
+
+export const withFirebaseAuthConsumer = createHoc(FirebaseAuthConsumer)({...config, firebase: firebase})
+
+export const withFirebaseDatabaseNode = createHoc(FirebaseDatabaseNode)
+
+export const withFirebaseDatabaseMutation = createHoc(FirebaseDatabaseMutation)
+
